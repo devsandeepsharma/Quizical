3fabc537a5c906ff55b3428fb260634afc4154a1
Sandeep <sandipsharma2445@gmail.com>
2025-06-05 09:13:46 +0530

    project started

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div className="flex justify-center items-center min-h-screen">
+            <h1 className="text-3xl">Quizical</h1>
+        </div>
+    )
+}
+
+export default App;
